7b9fc27c030d84bd192db264d010e9901b7bdad2
Pavan Chitradurga Thammanna <mail@pavanct.com>
2020-11-13 02:04:44 +0100

    add header component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
+  const API_KEY = process.env.API_KEY;
+  useEffect(() => {
+    return () => {};
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Header.css"
+
+function Header() {
+    return (
+        <div className="header">
+            <h1>SearchFlix</h1>
+            <h4>Search Movies and TV series</h4>
+        </div>
+    )
+}
+
+export default Header
