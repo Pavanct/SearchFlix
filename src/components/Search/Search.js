3a5afaa4a9ab7086ff15a4e284853bd8f7e263e7
Pavan Chitradurga Thammanna <mail@pavanct.com>
2020-11-13 15:58:21 +0100

    add carousal to trending

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,11 +1,19 @@
+import { TextField } from '@material-ui/core';
 import React from 'react'
+import "./Search.css"
 
 function Search() {
     return (
-        <div className="search">
-           Search
-        </div>
-    )
+      <div className="search">
+        <TextField
+          id="standard-full-width"
+          fullWidth
+          label="Search movies, tv series"
+          margin="normal"
+          color="primary"
+        />
+      </div>
+    );
 }
 
 export default Search
--- src/components/Trending/Trending.js
+++ src/components/Trending/Trending.js
@@ -9,73 +9,94 @@ import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import { flexbox } from '@material-ui/system';
 import "./Trending.css";
+import Carousel from 'react-elastic-carousel';
 import { Tooltip } from "@material-ui/core";
+import StarIcon from '@material-ui/icons/Star';
 
 const useStyles = makeStyles({
   root: {
     flexWrap: "wrap",
     flexDirection: "row",
-    maxWidth: 350,
+    width: 250,
     marginBottom: 30,
-    maxHeight: 700
+    height: 500,
+    backgroundColor: "black"
   },
   media: {
-    height: 500,
-    maxHeight: 600,
+    height: 350,
   },
 });
 
 function Trending({ trending }) {
   const classes = useStyles();
   const imgUrl = "https://image.tmdb.org/t/p/w500";
+  const breakPoints = [{ width: 1, itemsToShow: 1 },
+        { width: 550, itemsToShow: 2, itemsToScroll: 2, pagination: false },
+        { width: 850, itemsToShow: 3 },
+        { width: 1150, itemsToShow: 4, itemsToScroll: 2 },
+        { width: 1450, itemsToShow: 5 },
+        { width: 1750, itemsToShow: 6 },]
   return (
     <div className="trending">
-      {trending?.results?.map((element, index) => (
-        //   (console.log("trending", trending) ),
-        <Card className={classes.root} key={index}>
-          <CardActionArea>
-            <CardMedia
-              className={classes.media}
-              image={imgUrl + element?.poster_path}
-              title={element?.title}
-            />
-            <CardContent>
-              {element?.media_type !== "tv" &&
-                <Typography gutterBottom variant="h5" component="h2">
-                  {element?.original_title}
-                </Typography>
-              }{element?.name &&
-                <Typography gutterBottom variant="h5" component="h2">
-                  {element?.name}
-                </Typography>
-              }
-              <Tooltip title={<p style={{ fontSize: 14 }}>{element?.overview}</p>} enterDelay={800} css={{ fontSize: 16 }}>
+      <Carousel
+        itemsToScroll={2}
+        itemsToShow={6}
+        pagination={false}
+        easing={"ease"}
+        breakPoints={breakPoints}
+      >
+        {trending?.results
+          ?.filter((a) => a.original_language === "en")
+          .map((element, index) => (
+            <Card className={classes.root} key={index}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image={imgUrl + element?.poster_path}
+                  title={element?.title}
+                />
+                <CardContent className="card-content">
+                  {/* <Tooltip title={<p style={{ fontSize: 14 }}>{element?.overview}</p>} enterDelay={800} css={{ fontSize: 16 }}>
                 <Typography variant="body2" align="justify" paragraph={true} color="black" component="p" className="overview">
                   {element?.overview}
                 </Typography>
-              </Tooltip>
-              
-              {element?.media_type == "tv" &&
-                < Typography align="left" variant="subtitle2" color="textSecondary" component="p">
-                  {element?.media_type.toUpperCase()} series
-                </Typography>
-              }
-              {element?.media_type == "movie" &&
-                < Typography align="left" variant="subtitle2" color="textSecondary" component="p">
-                  {element?.media_type.toUpperCase()}
-                </Typography>
-              }
+              </Tooltip> */}
 
-              <Typography variant="subtitle2" align="right" color="textSecondary" >
-                {element?.vote_average}
-              </Typography>
-              <Typography variant="subtitle2" align="right" color="textSecondary" >
-                {element?.vote_count}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-      ))}
+                  {element?.media_type !== "tv" && (
+                    <Typography gutterBottom variant="h6" component="h5">
+                      {element?.original_title}
+                    </Typography>
+                  )}
+                  {element?.name && (
+                    <Typography gutterBottom variant="h6" component="h5">
+                      {element?.name}
+                    </Typography>
+                  )}
+
+                  {element?.media_type === "tv" && (
+                    <Typography variant="subtitle2" component="p">
+                      {element?.media_type.toUpperCase()} series
+                    </Typography>
+                  )}
+
+                  {element?.media_type === "movie" && (
+                    <Typography variant="subtitle2" component="p">
+                      {element?.media_type.toUpperCase()}
+                    </Typography>
+                  )}
+
+                  <Typography variant="subtitle2">
+                    <StarIcon style={{ color: "yellow", fontSize: 16 }} />{" "}
+                    {element?.vote_average}
+                  </Typography>
+                  {/* <Typography variant="subtitle2" align="right" >
+                  {element?.vote_count}
+                </Typography> */}
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          ))}
+      </Carousel>
     </div>
   );
 }
