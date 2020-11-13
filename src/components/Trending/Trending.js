ec07c251a2a70cb24742bc26402b1433dbff4e9d
Pavan Chitradurga Thammanna <mail@pavanct.com>
2020-11-13 05:25:09 +0100

    display cards with flex

--- src/components/Trending/Trending.js
+++ src/components/Trending/Trending.js
@@ -7,10 +7,17 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import { flexbox } from '@material-ui/system';
+import "./Trending.css";
+import { Tooltip } from "@material-ui/core";
 
 const useStyles = makeStyles({
   root: {
+    flexWrap: "wrap",
+    flexDirection: "row",
     maxWidth: 350,
+    marginBottom: 30,
+    maxHeight: 700
   },
   media: {
     height: 500,
@@ -33,22 +40,40 @@ function Trending({ trending }) {
               title={element?.title}
             />
             <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                {element?.title}
+              {element?.media_type !== "tv" &&
+                <Typography gutterBottom variant="h5" component="h2">
+                  {element?.original_title}
+                </Typography>
+              }{element?.name &&
+                <Typography gutterBottom variant="h5" component="h2">
+                  {element?.name}
+                </Typography>
+              }
+              <Tooltip title={<p style={{ fontSize: 14 }}>{element?.overview}</p>} enterDelay={800} css={{ fontSize: 16 }}>
+                <Typography variant="body2" align="justify" paragraph={true} color="black" component="p" className="overview">
+                  {element?.overview}
+                </Typography>
+              </Tooltip>
+              
+              {element?.media_type == "tv" &&
+                < Typography align="left" variant="subtitle2" color="textSecondary" component="p">
+                  {element?.media_type.toUpperCase()} series
+                </Typography>
+              }
+              {element?.media_type == "movie" &&
+                < Typography align="left" variant="subtitle2" color="textSecondary" component="p">
+                  {element?.media_type.toUpperCase()}
+                </Typography>
+              }
+
+              <Typography variant="subtitle2" align="right" color="textSecondary" >
+                {element?.vote_average}
               </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                {element?.overview}
+              <Typography variant="subtitle2" align="right" color="textSecondary" >
+                {element?.vote_count}
               </Typography>
             </CardContent>
           </CardActionArea>
-          <CardActions>
-            <Button size="small" color="primary">
-              Share
-            </Button>
-            <Button size="small" color="primary">
-              Learn More
-            </Button>
-          </CardActions>
         </Card>
       ))}
     </div>
