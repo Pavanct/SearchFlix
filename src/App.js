c520d110b5c47516168d23afcf33a5560779c561
Pavan Chitradurga Thammanna <mail@pavanct.com>
2020-11-13 03:56:18 +0100

    connect tmdb api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import { useEffect } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
+import Search from "./components/Search/Search";
+import Trending from "./components/Trending/Trending";
 
 function App() {
-  const API_KEY = process.env.API_KEY;
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const API_URL = "https://api.themoviedb.org/3/";
+  const [movies, setMovies] = useState();
+  const [trending, setTrending] = useState();
+
+  // call trending movies
   useEffect(() => {
-    return () => {};
+    (async () => {
+      let trendingEndpoint = API_URL + "trending/all/day?api_key=" + API_KEY;
+      await axios.get(trendingEndpoint).then((res) => {
+        console.log(res);
+        setTrending(res.data);
+      });
+    })();
   }, []);
 
   return (
     <div className="App">
-      <Header></Header>
+      <Header />
+      <Search />
+      <Trending trending={trending} />
     </div>
   );
 }
